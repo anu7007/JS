22a54d837b44e3b706e2618e0956c4dbd0ace7dc
Anupam Rastogi <anupamrastogi@cedcoss.com>
2022-02-05 14:55:31 +0530

    &&

--- script.js
+++ script.js
@@ -362,3 +362,19 @@ function testStrictNotEqual(val) {
   }
   
   testLessOrEqual(10);
+//
+function testLogicalAnd(val) {
+    // Only change code below this line
+  
+    if (val>=25 && val<=50) {
+      
+        return "Yes";
+      
+    }
+  
+    // Only change code above this line
+    return "No";
+  }
+  
+  testLogicalAnd(50);
+  //
