b2e5df440549440344004a27fc4d4642f35459b0
Anupam Rastogi <anupamrastogi@cedcoss.com>
2022-02-05 14:40:59 +0530

    ifs

--- script.js
+++ script.js
@@ -249,3 +249,15 @@ function processArg(num) {
   
     // Only change code above this line
   }
+//
+function trueOrFalse(wasThatTrue) {
+
+     if(wasThatTrue)
+     {
+       return "Yes, that was true";
+     }
+     return "No, that was false";
+  
+  }
+  //
+  
