96347415b33c5b804db8d7838a3364b5ea73e3cc
Anupam Rastogi <anupamrastogi@cedcoss.com>
2022-02-05 10:48:26 +0530

    understanding uninitialized variables

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 //this is an in-line comment
 /* this is a 
 multi-line comment*/
-var myName;
-// Setup
+// var myName;
+// // Setup
+// var a;
+// a=7;
+// var b;
+// b=a;
+// var myFirstName='a';
+// var myLastName='b';
+// Only change code below this line
 var a;
-a=7;
 var b;
-b=a;
-var myFirstName='a';
-var myLastName='b';
+var c;
+a=5;
+b=10;
+c="I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
