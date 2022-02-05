6f0fbea222b725b02b6a00917e45e2e3c7f2595c
Anupam Rastogi <anupamrastogi@cedcoss.com>
2022-02-05 14:43:42 +0530

    ===

--- script.js
+++ script.js
@@ -268,3 +268,12 @@ function trueOrFalse(wasThatTrue) {
   }
   
   testEqual(10);
+  //
+  function testStrict(val) {
+    if (val===7) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  testStrict(10);
