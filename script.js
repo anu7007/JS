db7fc52fe807175709af724bcaff662e6f104f4e
Anupam Rastogi <anupamrastogi@cedcoss.com>
2022-02-05 14:45:56 +0530

    !=

--- script.js
+++ script.js
@@ -286,3 +286,13 @@ function trueOrFalse(wasThatTrue) {
   }
   
   compareEquality(10, "10");
+  //
+  // Setup
+function testNotEqual(val) {
+    if (val!="99") { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testNotEqual(10);
