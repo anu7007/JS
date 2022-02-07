91b28c5c9caa03f41892c8151b5d66b76c38d000
Anupam Rastogi <anupamrastogi@cedcoss.com>
2022-02-07 11:32:07 +0530

    delete properties from js objects

--- script.js
+++ script.js
@@ -685,4 +685,15 @@ const myDog = {
   "friends": ["freeCodeCamp Campers"]
 };
 myDog.bark="woof";
+//
+// Setup
+const myDog = {
+  "name": "Happy Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"],
+  "bark": "woof"
+};
 
+// Only change code below this line
+delete myDog.tails;
