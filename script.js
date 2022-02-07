130ee32effba0bb7547da332e7e4f0a8fd93f6d8
Anupam Rastogi <anupamrastogi@cedcoss.com>
2022-02-07 12:06:51 +0530

    iterate in for loop

--- script.js
+++ script.js
@@ -852,3 +852,12 @@ const myArray = [];
 for (let i = 9; i > 0; i -= 2) {
   myArray.push(i);
 }
+//
+// Setup
+const myArr = [2, 3, 4, 5, 6];
+
+// Only change code below this line
+var total=0;
+for (let i = 0; i < myArr.length; i++) {
+   total+=myArr[i];
+}
