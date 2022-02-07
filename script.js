788b2a4a313967d9ba7993e0ba3ac3466ea6f0f1
Anupam Rastogi <anupamrastogi@cedcoss.com>
2022-02-07 11:51:22 +0530

    accessing nested objects

--- script.js
+++ script.js
@@ -747,3 +747,17 @@ const myMusic = [
   }
 
 ];
+//
+const myStorage = {
+  "car": {
+    "inside": {
+      "glove box": "maps",
+      "passenger seat": "crumbs"
+     },
+    "outside": {
+      "trunk": "jack"
+    }
+  }
+};
+
+const gloveBoxContents = myStorage.car.inside["glove box"];
