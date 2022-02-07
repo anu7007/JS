c5c95edf18eb05819227dddee70f512935685e64
Anupam Rastogi <anupamrastogi@cedcoss.com>
2022-02-07 11:22:55 +0530

    accessing object values with bracket notation

--- script.js
+++ script.js
@@ -645,3 +645,13 @@ const testObj = {
 const hatValue = testObj.hat;     
 const shirtValue = testObj.shirt; 
 //
+// Setup
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+const entreeValue = testObj["an entree"];
+const drinkValue = testObj["the drink"]; 
