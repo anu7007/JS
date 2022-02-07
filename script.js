c4386893ea9558330851e6cbfbf7aa1bed49f77e
Anupam Rastogi <anupamrastogi@cedcoss.com>
2022-02-07 11:29:29 +0530

    updating object properties

--- script.js
+++ script.js
@@ -666,3 +666,14 @@ const testObj = {
 // Only change code below this line
 const playerNumber = 16;
 const player = testObj[playerNumber];
+//
+// Setup
+const myDog = {
+  "name": "Coder",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["freeCodeCamp Campers"]
+};
+
+// Only change code below this line
+myDog["name"]="Happy Coder";
