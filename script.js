4180b0e7f3931dee299ef0dd38fcb5c4cf6df093
Anupam Rastogi <anupamrastogi@cedcoss.com>
2022-02-07 11:27:52 +0530

    accessing object values with variables

--- script.js
+++ script.js
@@ -655,3 +655,14 @@ const testObj = {
 // Only change code below this line
 const entreeValue = testObj["an entree"];
 const drinkValue = testObj["the drink"]; 
+//
+// Setup
+const testObj = {
+  12: "Namath",
+  16: "Montana",
+  19: "Unitas"
+};
+
+// Only change code below this line
+const playerNumber = 16;
+const player = testObj[playerNumber];
