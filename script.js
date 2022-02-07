7e677228c56b31bf8aeee756ce5ce03a82a2243d
Anupam Rastogi <anupamrastogi@cedcoss.com>
2022-02-07 09:40:28 +0530

    default in switch

--- script.js
+++ script.js
@@ -504,3 +504,27 @@ switch(val){
 }
 
 caseInSwitch(1);
+//
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+ switch(val){
+case 'a':
+return "apple";
+break;
+case 'b':
+return "bird";
+break;
+case 'c':
+return "cat";
+break;
+default:
+return "stuff";
+ }
+
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
